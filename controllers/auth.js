e12f94e260785e4f47416c90330eaaf6ff50ecba
Aleksandr <alexbachurin91@gmail.com>
2023-03-23 14:12:50 +0300

    create token and send info back to frontend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const UserModel = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
+  // create user in db with model
   const user = await UserModel.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  // create token
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+  // send back to front-end user object with name and token
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
