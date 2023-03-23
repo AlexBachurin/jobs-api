10489464ae4bb33d3192fc07ea939ed7c03c4423
Aleksandr <alexbachurin91@gmail.com>
2023-03-23 13:58:45 +0300

    hash user password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,16 @@
 const UserModel = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  const user = await UserModel.create({ ...req.body });
+  const { name, password, email } = req.body;
+  //generate random bytes for hashing
+  const salt = await bcrypt.genSalt(10);
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // change password for user to hashed password
+  const tempUser = { name, password: hashedPassword, email };
+  const user = await UserModel.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -22,7 +22,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 4,
-    maxlength: 12,
   },
 });
 
