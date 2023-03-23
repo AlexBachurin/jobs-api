5b749fac52f59778748b43e9cf4cd1967c30b3fe
Aleksandr <alexbachurin91@gmail.com>
2023-03-23 21:15:12 +0300

    create job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,16 +1,27 @@
+const JobModel = require("../models/Job");
+const { BadRequestError, NotFoundError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+// Get All Jobs
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
-
+// Get Single Job
 const getJob = async (req, res) => {
   res.send("get single job");
 };
+// Create Job
 const createJob = async (req, res) => {
-  res.send(req.user);
+  // add property of createdBy to req.body and use user id we get from auth middleware
+  req.body.createdBy = req.user.userId;
+  // create job
+  const job = await JobModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+// Edit Job
 const updateJob = async (req, res) => {
   res.send("update job");
 };
+// Delete job
 const deleteJob = async (req, res) => {
   res.send("delete job");
 };
